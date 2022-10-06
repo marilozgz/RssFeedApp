6439f31b5f6e62f71cd1f6be0be703e701eb374c
marilozgz <crazygipsypuaj@gmail.com>
2022-10-07 00:46:22 +0200

    added comments

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,30 +1,21 @@
-
+//function to get the image from the html content
 export const imageFromHtmlFeed = (html) => {
-  var regex = /(https?:\/\/[^ ]*\.(?:gif|png|jpg|jpeg))/i;
-
-  if (new RegExp(regex).test(html)) {
-    let imgurl = regex.exec(html)[1];
-
-    if (imgurl) return imgurl;
-  } else {
-    return null;
-  }
+  const regex = /(https?:\/\/[^ ]*\.(?:gif|png|jpg|jpeg))/i;
+  const imgurl = new RegExp(regex).test(html) ? regex.exec(html)[1] : null;
+  return imgurl;
 };
 
+//function to format the html content to show 200 characters, and remove the html tags
 export const formatHTML = (html) => {
-  if (html === null || html === "") return false;
-  else html = html.toString();
-
-  const stringWithoutTags=  html.replace(/(<([^>]+)>)/gi, "");
+  (html === null || html === "") ? false :html.toString();
+  const stringWithoutTags = html.replace(/(<([^>]+)>)/gi, "");
   return stringWithoutTags.substring(0, 200).trim();
 };
 
-
+//function to format the html content to show the full content, and remove the html tags
 export const fullHtml = (html) => {
-  if (html === null || html === "") return false;
-  else html = html.toString();
-
-  const stringWithoutTags=  html.replace(/(<([^>]+)>)/gi, "");
-  const nolinks = stringWithoutTags.split('more')[0]+'';
+  (html === null || html === "") ? false : html.toString();
+  const stringWithoutTags = html.replace(/(<([^>]+)>)/gi, "");
+  const nolinks = stringWithoutTags.split("more")[0] + "";
   return nolinks.trim();
 };
